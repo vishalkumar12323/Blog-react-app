3cd5ba8c3083305684a990587b46f7b55629f5aa
vishal <vishalninaniya8949info@gmail.com>
2024-06-10 18:10:11 +0530

    create and setup blog app with react & tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-green-300 p-2">
+        Mega Project Called Blog With Appwriter
+      </h1>
+    </>
+  );
+}
+
+export default App;
