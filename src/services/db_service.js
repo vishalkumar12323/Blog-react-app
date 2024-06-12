59e5f8fec28d3cd61779fcf2aaab08f6d0b10285
vishal <vishalninaniya8949info@gmail.com>
2024-06-12 10:09:49 +0530

    complete databases services integartion

--- src/services/db_service.js
+++ src/services/db_service.js
@@ -55,14 +55,66 @@ class DatabaseService {
 
   async deleteBlog(slug) {
     try {
-      return await this.databases.deleteDocument(
+      await this.databases.deleteDocument(
         config.appwrite_database_id,
         config.appwrite_collection_id,
         slug
       );
+      return true;
     } catch (error) {
       console.log("Database services :: delete document errors", error);
+      return false;
+    }
+  }
+
+  async getBlog(slug) {
+    try {
+      const blog = await this.databases.getDocument(
+        config.appwrite_database_id,
+        config.appwrite_collection_id,
+        slug
+      );
+      return blog;
+    } catch (error) {
+      console.log("Database Services :: error fetching blog ", error);
       throw error;
+    }
+  }
+
+  async getAllBlog() {
+    try {
+      return await this.databases.listDocuments(
+        config.appwrite_database_id,
+        config.appwrite_collection_id
+      );
+    } catch (error) {
+      console.log("Database Services :: error fetching all blog ", error);
+      throw error;
+    }
+  }
+
+  // files uploads services
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        config.appwrite_bucket_id,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Storage Services :: error uploading file ", error);
+      throw error;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(config.appwrite_bucket_id, fileId);
+      return true;
+    } catch (error) {
+      console.log("Storage Services :: error deleting file ", error);
+      return false;
     }
   }
 }
