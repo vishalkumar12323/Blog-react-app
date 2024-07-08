5d0ca6a65d1c4f47a93c312ef29f50402e33f589
vishal <vishalninaniya8949info@gmail.com>
2024-07-08 14:33:54 +0530

    adding slider component

--- src/components/main/BlogSlider.jsx
+++ src/components/main/BlogSlider.jsx
@@ -1,11 +1,21 @@
 import clsx from "clsx";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
 
 const BlogSlider = ({ blogs }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const length = blogs.length;
-  const nextSlide = () => {
-    setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1);
+  const currentSlideRef = useRef(null);
+  const nextSlide = (id) => {
+    console.log(currentSlideRef.current.attributes.id);
+    // setCurrentIndex((previousIndex) => {
+    //   if (previousIndex === length - 1) {
+    //     return 0;
+    //   } else {
+    //     currentSlideRef.current.style.transform = "translateX(-100%)";
+    //     return previousIndex + 1;
+    //   }
+    // });
   };
 
   // useEffect(() => {
@@ -17,44 +27,51 @@ const BlogSlider = ({ blogs }) => {
   //   return () => clearInterval(interval);
   // }, [nextSlide]);
   return (
-    <>
-      <section className="py-4">
-        <div className="slider w-full relative">
+    <section className="w-full h-screen flex items-start">
+      <div className="w-full h-full my-1">
+        <div className="w-full h-full relative overflow-hidden">
           {blogs.map((blog) => (
             <div
               className={clsx(
-                `w-full h-screen absolute top-0 left-0 transition-opacity duration-500`
+                `w-full h-full absolute top-0 left-0 translate-x-full transition-transform`,
+                {
+                  "translate-x-0": currentIndex === blog.id,
+                }
               )}
               key={blog.id}
-              style={{
-                backgroundImage: `url(${blog.coverImage})`,
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                opacity: currentIndex === blog.id ? "1" : "0",
-              }}
+              ref={currentSlideRef}
+              id={blog.id}
             >
-              <div className="">
-                <div className="flex">
-                  <div className="">
-                    <div className="blog-content text-black">
-                      <h2>{blog.heading}</h2>
-                      <p>{blog.content}</p>
-                      <a href={blog.link} className="">
-                        Read More {blog.id}
-                      </a>
-                    </div>
-                  </div>
+              <img
+                src={blog.coverImage}
+                alt="bla"
+                className="w-full h-auto select-none opacity-60"
+              />
+              <div className="content text-white absolute top-[60%] left-10 flex flex-col gap-4">
+                <h3 className="text-6xl text-start">{blog.heading}</h3>
+                <div className="mt-3">
+                  <Link
+                    to="#"
+                    className="px-6 py-2 bg-green-600 capitalize w-fit rounded-sm hover:bg-green-500 transition-colors"
+                  >
+                    read more {blog.id}
+                  </Link>
                 </div>
               </div>
             </div>
           ))}
-          <button onClick={nextSlide} className="absolute left-1/2 bottom-0">
-            next
-          </button>
+
+          <div className="absolute bottom-5 left-1/2 flex gap-3">
+            <button
+              className="w-4 h-4 bg-green-600 border rounded-full"
+              onClick={nextSlide}
+            ></button>
+            <button className="w-4 h-4 bg-green-600 border rounded-full"></button>
+            <button className="w-4 h-4 bg-green-600 border rounded-full"></button>
+          </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
